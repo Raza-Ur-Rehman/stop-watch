e8e0eac0716b0490cc3537d43f0ea015507a4583
Raza ur Rehman <mrazaurrehman12345@gmail.com>
2024-06-05 00:38:59 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 let seconds = 0;
 let tens = 0;
+let mint = 0;
+let hour = 0;
 let getSeconds = document.querySelector(".seconds");
 let getTens = document.querySelector(".tens");
+let getMint = document.querySelector(".mint");
+let getHour = document.querySelector(".hour");
 let btnStart = document.querySelector(".btn-start");
 let btnStop = document.querySelector(".btn-stop");
 let btnReset = document.querySelector(".btn-reset");
@@ -24,8 +28,12 @@ btnReset.addEventListener("click", () => {
 
   tens = "00";
   seconds = "00";
+  mint = "00";
+  hour = "00";
   getSeconds.innerHTML = seconds;
   getTens.innerHTML = tens;
+  getMint.innerHTML = mint;
+  getHour.innerHTML = hour;
 });
 
 function startTimer() {
@@ -44,5 +52,15 @@ function startTimer() {
   }
   if (seconds > 9) {
     getSeconds.innerHTML = seconds;
+  }
+  if (seconds > 60) {
+    mint++;
+    getMint.innerHTML = "0" + mint;
+    seconds = 0;
+    getSeconds.innerHTML = "0" + 0
+
+  }
+  if (mint > 9) {
+    getMint.innerHTML = mint;
   }
 }
